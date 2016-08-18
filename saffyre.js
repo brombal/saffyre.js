761cce218708dc3538450b71675ea582f8bcd510
Alex Brombal <alex@brombal.com>
2016-08-18 14:42:04 -0500

    Add support for ES6-style modules

--- saffyre.js
+++ saffyre.js
@@ -8,43 +8,41 @@ module.exports = function (dir) {
 
     var saffyre = express.Router();
 
-    function loadFiles() {
-        readdir(dir, function(e, files) {
+    readdir(dir, function (e, files) {
 
-            files = _.sortBy(_.map(files, (file) => {
-                var path = file.replace(dir + '/', '');
-                return {
-                    segments: path.replace(/\.js$/, '').split('/'),
-                    path: path.replace(/\.js$/, ''),
-                    abspath: file
-                }
-            }, (file) => {
-                return file.segments.length;
-            })).reverse();
+        files = _.sortBy(_.map(files, (file) => {
+            var path = file.replace(dir + '/', '');
+            return {
+                segments: path.replace(/\.js$/, '').split('/'),
+                path: path.replace(/\.js$/, ''),
+                abspath: file
+            }
+        }, (file) => {
+            return file.segments.length;
+        })).reverse();
 
-            files = _.partition(files, file => file.segments[file.segments.length - 1] == '_global');
+        files = _.partition(files, file => file.segments[file.segments.length - 1] == '_global');
 
-            files[0].forEach(file => {
-                var router = require(file.abspath);
-                if (router) {
-                    file.segments.pop();
-                    saffyre.use('/' + file.segments.join('/'), router);
-                }
-            });
-
-            files[1].forEach(file => {
-                if (file.segments[file.segments.length - 1] == '_default')
-                    file.segments.pop();
-
-                var router = require(file.abspath);
-                if (router && router.name == 'router') {
-                    saffyre.use('/' + file.segments.join('/'), router);
-                }
-            });
+        files[0].forEach(file => {
+            var router = require(file.abspath);
+            if (router) {
+                router = router.default || router;
+                file.segments.pop();
+                saffyre.use('/' + file.segments.join('/'), router);
+            }
         });
-    }
 
-    loadFiles();
+        files[1].forEach(file => {
+            if (file.segments[file.segments.length - 1] == '_default')
+                file.segments.pop();
+
+            var router = require(file.abspath);
+            router = (router && router.default) || router;
+            if (router && router.name == 'router') {
+                saffyre.use('/' + file.segments.join('/'), router);
+            }
+        });
+    });
 
     return saffyre;
 };
